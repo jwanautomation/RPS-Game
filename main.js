d3e310fb5c5f09e1944022839658a317dee6586b
Jun Wan <jwanautomation@gmail.com>
2022-08-22 16:04:35 +0100

    Test 1 - User validation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ROCK PAPER SCISSORS GAME
 
-// USERCHOICE FUNCTION
+// USERCHOICE - FUNCTION 1
 const getUserChoice = (userInput) => {
   // LOWER CASE FUNCTION
   userInput = userInput.toLowerCase();
@@ -17,3 +17,16 @@ const getUserChoice = (userInput) => {
     console.log("Error. Try using rock, paper or scissors");
   }
 };
+
+// TEST 1 - USER VALIDATION
+
+// VALID INPUT ROCK
+// console.log(getUserChoice("rock"));
+// VALID INPUT PAPER
+// console.log(getUserChoice("paper"));
+// VALID INPUT SCISSORS
+// console.log(getUserChoice("scissors"));
+// VALID INPUT BOMB (CHEAT CODE)
+// console.log(getUserChoice("bomb"));
+// INVALID INPUT APPLE
+// console.log(getUserChoice("apple"));
